bba9e557e1e65b8b38923f952068d6a3d9a4b8f8
shantimohan <shantimohan@outlook.com>
2018-01-03 13:18:41 -0500

    Add project files.

---
+++ SimpleApp/SimpleApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+// This is a practice app to get started with Xamarin and using LivePlayer
+//    following steps as described at https://aka.ms/get-started for the contest
+//    as detailed in this blog post https://blog.xamarin.com/contest-get-started-xamarin/
+
+namespace SimpleApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Button1_Clicked(Object sender, EventArgs e)
+        {
+            Label1.Text = "Monkey is Ready to Fly!!!";
+        }
+
+    }
+}
